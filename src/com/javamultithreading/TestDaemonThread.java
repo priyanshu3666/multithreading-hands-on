aa7d9ec410065fedc1ebb9775b114ef21c5e0e85
Priyanshu Shukla <priyanshu.shukla_cs18@gla.ac.in>
2021-11-10 09:56:20 +0530

    implementing daemon thread  hands-on

---
+++ src/com/javamultithreading/TestDaemonThread.java
@@ -0,0 +1,24 @@
+package com.javamultithreading;
+
+public class TestDaemonThread extends Thread{
+    @Override
+    public void run(){
+        if(Thread.currentThread().isDaemon()){//checking for daemon thread
+            System.out.println("daemon thread work");
+        }
+        else{
+            System.out.println("user thread work");
+        }
+    }
+    public static void main(String[] args){
+        TestDaemonThread t1=new TestDaemonThread();
+        TestDaemonThread t2=new TestDaemonThread();
+        TestDaemonThread t3=new TestDaemonThread();
+
+        t1.setDaemon(true);//now t1 is daemon thread
+
+        t1.start();
+        t2.start();
+        t3.start();
+    }
+}
